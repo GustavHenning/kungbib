c928c0dfeb4b7dd37786fd7e7e7a60016b0c647c
Gustav Henning <ghenning@kth.se>
2021-09-22 16:25:02 +0200

    started outline for kb experiments

---
+++ keras/kb/kb_vanilla.py
@@ -0,0 +1,8 @@
+# TODO implement kb vanilla with 3 channels
+# TODO implement kb ndim with 3 + n channels
+# TODO make a script for downloading and verifying text corresponding to image. Support different methods of vectorization
+# TODO design architecture so experiments can be run using 
+# a) vanilla or ndim(n=x), 
+# b) different datasets, 
+# c) output logs and corresponding visualizations to a directory (modify model.py to return history then https://valueml.com/how-to-visualize-the-training-process-in-keras/)
+# based on config (a, b, c)
