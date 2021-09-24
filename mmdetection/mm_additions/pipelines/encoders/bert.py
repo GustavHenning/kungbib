9c875590d4622dd55d7bbdc71ee26de18d783ae5
Gustav Henning <ghenning@kth.se>
2021-09-24 16:24:48 +0200

    fixed typo and added a todo

--- mmdetection/mm_additions/pipelines/encoders/bert.py
+++ mmdetection/mm_additions/pipelines/encoders/bert.py
@@ -3,11 +3,11 @@ import torch
 from sentence_transformers import SentenceTransformer
 
 class BERT(Encoder):
-    def __init__(self, dimensions, model_name='all-MiniLM-L6-v2'):
+    def __init__(self, dimensions, model_name='all-MiniLM-L6-v2'): # TODO other models?
         self.dimensions = dimensions
         self.model = SentenceTransformer(model_name, device="cpu") # cpu because we are already using the cuda device :(
     """
-    Returns an (1,n)-dimensional array of vectorzied text where n is the dimensions passed to the constructor
+    Returns an (1,n)-dimensional array of vectorized text where n is the dimensions passed to the constructor
     """
     def encode(self, text):
         return self.model.encode(text)[0:self.dimensions] # TODO original vector is 384 dimensions, how to compress into n?
