cbbb46a8c489ccbb6ba11fbc26a4ada28158ddd7
Gustav Henning <ghenning@kth.se>
2021-12-02 15:59:01 +0100

    fix path for weights

--- mmdetection/mm_additions/configs/gustav/kungbib-cascade-mask-101-32x4d-tf.py
+++ mmdetection/mm_additions/configs/gustav/kungbib-cascade-mask-101-32x4d-tf.py
@@ -7,5 +7,5 @@ model = dict(
         bbox_head=dict(num_classes=NUM_CLASSES),
         mask_head=dict(num_classes=NUM_CLASSES)))
 
-load_from = 'checkpoints/mask_rcnn_x101_32x4d_fpn_mstrain-poly_3x_coco_20210524_201410-abcd7859.pth'
+load_from = 'checkpoints/full/mask_rcnn_x101_32x4d_fpn_mstrain-poly_3x_coco_20210524_201410-abcd7859.pth'
 
