330d335e8508d7ab19f982f8465d0ef7b6cfefc7
Gustav Henning <nightmunnas@gmail.com>
2022-01-07 15:31:53 +0100

    name of pdf changed

--- latex/kththesis/compile.sh
+++ latex/kththesis/compile.sh
@@ -1,1 +1,1 @@
-pdflatex examplethesis.tex; pdflatex examplethesis.tex; bibtex examplethesis; makeglossaries examplethesis; pdflatex examplethesis.tex
+pdflatex examplethesis.tex; pdflatex examplethesis.tex; bibtex examplethesis; makeglossaries examplethesis; pdflatex examplethesis.tex; cp examplethesis.pdf Gustav_Henning.pdf
