7a2a59b32e44a6abd25aaad59b2683dbcd2f8bac
Gustav Henning <ghenning@kth.se>
2022-01-13 11:03:42 +0100

    train doc2vec with boolean and some dims

--- mmdetection/train_doc2vec.sh
+++ mmdetection/train_doc2vec.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
 bash train_tf.sh kungbib-cascade-mask-tf doc2vec 1 doc2vec 1
-bash train_tf.sh kungbib-cascade-mask-tf doc2vec 4 doc2vec 1
-bash train_tf.sh kungbib-cascade-mask-tf doc2vec 16 doc2vec 1
-bash train_tf.sh kungbib-cascade-mask-tf doc2vec 32 doc2vec 1
+bash train_tf.sh kungbib-cascade-mask-tf doc2vec 2 doc2vec 1
+bash train_tf.sh kungbib-cascade-mask-tf doc2vec 3 doc2vec 1
 
-bash train_tf.sh kungbib-cascade-mask-tf doc2vec 1 doc2vec 2
-bash train_tf.sh kungbib-cascade-mask-tf doc2vec 4 doc2vec 2
-bash train_tf.sh kungbib-cascade-mask-tf doc2vec 16 doc2vec 2
-bash train_tf.sh kungbib-cascade-mask-tf doc2vec 32 doc2vec 2
+bash train_tf.sh kungbib-cascade-mask-tf doc2vec 1 boolean 1
+bash train_tf.sh kungbib-cascade-mask-tf doc2vec 2 boolean 1
+bash train_tf.sh kungbib-cascade-mask-tf doc2vec 3 boolean 1
+#bash train_tf.sh kungbib-cascade-mask-tf doc2vec 4 doc2vec 1
+#bash train_tf.sh kungbib-cascade-mask-tf doc2vec 16 doc2vec 1
+#bash train_tf.sh kungbib-cascade-mask-tf doc2vec 32 doc2vec 1
+
+#bash train_tf.sh kungbib-cascade-mask-tf doc2vec 1 doc2vec 2
+#bash train_tf.sh kungbib-cascade-mask-tf doc2vec 4 doc2vec 2
+#bash train_tf.sh kungbib-cascade-mask-tf doc2vec 16 doc2vec 2
+#bash train_tf.sh kungbib-cascade-mask-tf doc2vec 32 doc2vec 2
 
 
 bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 1 doc2vec 1
-bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 4 doc2vec 1
-bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 16 doc2vec 1
-bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 32 doc2vec 1
+bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 2 doc2vec 1
+bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 3 doc2vec 1
 
-bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 1 doc2vec 2
-bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 4 doc2vec 2
-bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 16 doc2vec 2
-bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 32 doc2vec 2
+bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 1 boolean 1
+bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 2 boolean 1
+bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 3 boolean 1
+#bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 4 doc2vec 1
+#bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 16 doc2vec 1
+#bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 32 doc2vec 1
+
+#bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 1 doc2vec 2
+#bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 4 doc2vec 2
+#bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 16 doc2vec 2
+#bash train_tf.sh kungbib-cascade-mask-tf-1c doc2vec 32 doc2vec 2
 
 # https://link.springer.com/chapter/10.1007/978-3-319-67008-9_16
